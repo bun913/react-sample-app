dc67f42ecec590a873217cf1abf2fe787e9882f4
bun913 <nagasima.romance+@gmail.com>
2023-07-23 17:01:40 +0900

    feat: user reducer

--- src/components/useState.tsx
+++ src/components/useState.tsx
@@ -1,22 +1,33 @@
-import { useState } from "react";
+import { useReducer } from "react";
+
+type Action = "DECREMENT" | "INCREMENT" | "DOUBLE" | "RESET";
+
+const reducer = (currentCount: number, action: Action) => {
+  switch (action) {
+    case "DECREMENT":
+      return currentCount - 1;
+    case "INCREMENT":
+      return currentCount + 1;
+    case "DOUBLE":
+      return currentCount * 2;
+    case "RESET":
+      return 0;
+  }
+};
 
 type CounterProps = {
   initialValue: number;
 };
 
 export const Counter = (props: CounterProps) => {
-  const [value, setValue] = useState<number>(props.initialValue);
-  const increment = () => {
-    setValue((prevState) => prevState + 1);
-  };
-  const decrement = () => {
-    setValue((prevState) => prevState - 1);
-  };
+  const [count, dispatch] = useReducer(reducer, props.initialValue);
   return (
     <div>
-      <div>value: {value}</div>
-      <button onClick={increment}>+1</button>
-      <button onClick={decrement}>-1</button>
+      <p>{count}</p>
+      <button onClick={() => dispatch("DECREMENT")}>-</button>
+      <button onClick={() => dispatch("INCREMENT")}>+</button>
+      <button onClick={() => dispatch("DOUBLE")}>x2</button>
+      <button onClick={() => dispatch("RESET")}>Reset</button>
     </div>
   );
 };
